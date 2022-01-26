96751d31bdab5c1d0d6cd1ef5c937abe584a857d
MHZ <mohamed.hichem.zayani@gmail.com>
2022-01-27 00:21:41 +0100

    add escape sequences branch

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        String ch = "Hello World !";
-
-        System.out.println("ends with '!' : " + ch.endsWith("!"));
-        System.out.println("starts with 'h' : " + ch.startsWith("h"));
-        System.out.println("length : " + ch.length());
-        System.out.println("index of 'o' : " + ch.indexOf("o"));
-        System.out.println("replace every 'o' with 'a' : " + ch.replace("o","a"));
-        System.out.println("to lowercase : " + ch.toLowerCase());
-        System.out.println("to uppercase : " + ch.toUpperCase());
-        ch = "    Hello World !    ";
-        System.out.println("trim : " + ch.trim());
+        System.out.println("\"Hello World\"");
+        // "Hello World"
+        System.out.println("C:\\programs\\folder");
+        // C:\programs\folder
+        System.out.println("new\nline");
+        // new
+        // line
+        System.out.println("\ttab");
+        //     tab
     }
 }
