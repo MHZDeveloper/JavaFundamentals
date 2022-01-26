0d859caf9996a0bdc0abd6980f4ca6b30bce5e96
MHZ <mohamed.hichem.zayani@gmail.com>
2022-01-27 00:34:58 +0100

    add constants branch

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-        // byte[][] myNumbers = new byte[2][2];
-        byte[][] myNumbers = {{4, 1}, {5, 2}};
-        System.out.println("first element = " + myNumbers[0][0]);
-        myNumbers[0][0] = 3;
-        System.out.println("length = " + myNumbers.length);
-        System.out.println("myNumbers = " + Arrays.deepToString(myNumbers));
+        final float PI = 3.14F;
+        System.out.println(PI);
     }
 }
