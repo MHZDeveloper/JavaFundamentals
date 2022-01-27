da1a47802b4e7fb3b919f242cb845a4a5f0e2864
MHZ <mohamed.hichem.zayani@gmail.com>
2022-01-27 15:45:20 +0100

    add assignment 3 branch

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-        // example 1
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
-        }
 
-        // example 2
-        int i = 1;
-        do {
-            System.out.println(i);
-            i++;
-        } while (i <= 10);
+        for (int i=1;i<=100;i++){
+            String msg="";
+            if (i%3==0)
+                msg += "Foo";
+            if (i%5==0)
+                msg += "Bar";
+            if (i%7==0)
+                msg += "Qix";
 
-        //example 3
-        int j = 1;
-        while (j <= 10) {
-            System.out.println(j);
-            j++;
+            if (msg.isEmpty())
+                System.out.println(i);
+            else
+                System.out.println(msg);
         }
     }
 }
