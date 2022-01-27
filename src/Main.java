d592afefc51eff82c316e43c357a9c41cbfa3998
MHZ <mohamed.hichem.zayani@gmail.com>
2022-01-28 00:20:25 +0100

    add encapsulation branch

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         TextBox textBox = new TextBox();
-        textBox.text = "Hello World !";
-        System.out.println(textBox.text);
+        textBox.setText("Hello World !");
+        System.out.println(textBox.getText());
     }
 }
--- src/TextBox.java
+++ src/TextBox.java
@@ -1,17 +1,22 @@
 public class TextBox {
 
-    String text;
-    boolean isEnabled;
+    private String text;
+    private boolean isEnabled = true;
 
     void setText(String text) {
-
+        if (isEnabled)
+            this.text = text;
     }
 
     void enable() {
-
+        this.isEnabled = true;
     }
 
     void disable() {
+        this.isEnabled = false;
+    }
 
+    public String getText() {
+        return text;
     }
 }
