98a17813c5387092d4d535b761c6c8f800bef380
MHZ <mohamed.hichem.zayani@gmail.com>
2022-01-27 15:21:05 +0100

    add switch statement

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        short myNumber = -2;
+        short myNumber = 2;
         String msg = "";
         if (myNumber > 0) {
             msg = "Positive";
@@ -11,7 +11,23 @@ public class Main {
             msg = "Zero";
         }
         System.out.println(msg);
+
         msg = myNumber % 2 == 0 ? "Even" : "Odd";
         System.out.println(msg);
+
+        switch (myNumber) {
+            case 1: {
+                System.out.println("1");
+                break;
+            }
+            case 2: {
+                System.out.println("2");
+                break;
+            }
+            case 3: {
+                System.out.println("3");
+                break;
+            }
+        }
     }
 }
