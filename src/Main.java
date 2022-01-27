04ebe884aba5fbe6df7d9063ef39889bb1bd7fa4
MHZ <mohamed.hichem.zayani@gmail.com>
2022-01-27 14:57:13 +0100

    add logical operators branch

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        short myNumber = 2;
-        boolean isEven = myNumber % 2 == 0;
-        System.out.println("even : " + isEven);
-        boolean isOdd = myNumber % 2 != 0;
-        System.out.println("odd : " + isOdd);
-        boolean isPositive = myNumber > 0;
-        System.out.println("positive : " + isPositive);
-        boolean isZero = myNumber == 0;
-        System.out.println("is not zero : " + isZero);
+        short myNumber = -2;
+        boolean isPositiveAndEven = myNumber > 0 && myNumber % 2 == 0;
+        boolean isPositiveOrEven = myNumber > 0 || myNumber % 2 == 0;
+        System.out.println("isPositiveAndEven = " + isPositiveAndEven);
+        System.out.println("isPositiveOrEven = " + isPositiveOrEven);
     }
 }
