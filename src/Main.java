2276297790a3d04085e74a1bec8007027df4da26
MHZ <mohamed.hichem.zayani@gmail.com>
2022-01-27 14:46:45 +0100

    add comparisons branch

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,14 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("ax² + bc + c = 0");
-
-        System.out.print("a = ");
-        double a = scanner.nextDouble();
-
-        System.out.print("b = ");
-        double b = scanner.nextDouble();
-
-        System.out.print("c = ");
-        double c = scanner.nextDouble();
-
-        double delta = Math.pow(b, 2) - (4 * a * c);
-        System.out.println("delta = " + delta);
+        short myNumber = 2;
+        boolean isEven = myNumber % 2 == 0;
+        System.out.println("even : " + isEven);
+        boolean isOdd = myNumber % 2 != 0;
+        System.out.println("odd : " + isOdd);
+        boolean isPositive = myNumber > 0;
+        System.out.println("positive : " + isPositive);
+        boolean isZero = myNumber == 0;
+        System.out.println("is not zero : " + isZero);
     }
 }
