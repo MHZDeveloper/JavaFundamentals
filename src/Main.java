a54c411312d0403646339dccc5e3846f0d1fab1b
MHZ <mohamed.hichem.zayani@gmail.com>
2022-01-27 01:32:22 +0100

    add casting branch

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        int a = 2;
-        int b = 3;
-        System.out.println("a + b = " + (a + b));
-        System.out.println("a - b = " + (a - b));
-        System.out.println("a * b = " + (a * b));
-        System.out.println("a / b = " + (a / b));
-        System.out.println("a % b = " + (a % b));
+        // byte -> short -> int -> long -> float -> double
+        byte x = 3;
+        int y =x;
+        x=(byte)y;
 
-        int x1 = 1;
-        int y1 = ++x1;
-        System.out.println("x1 = "+x1);
-        System.out.println("y1 = "+y1);
-
-        int x2 = 1;
-        int y2 = x2++;
-        System.out.println("x2 = "+x2);
-        System.out.println("y2 = "+y2);
-
-        int number = 5;
-        number+=3; // number = number + 3
-        number-=3; // number = number - 3
-        number*=3; // number = number * 3
-        number/=3; // number = number / 3
-        number%=3; // number = number % 3
+        float a = 1.2F;
+        double b = a;
+        a = (float)b;
     }
 }
